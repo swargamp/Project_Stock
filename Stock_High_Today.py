7234de5bf7577a1f919a32987a97f99acfbe3b58
Macbook Pro <macbookpro@MacBook.local>
2020-11-16 23:28:24 -0800

    Added_BeautifulSoap

--- Stock_High_Today.py
+++ Stock_High_Today.py
@@ -1,11 +1,11 @@
 import requests
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from bs4 import BeautifulSoup
 
 def Making_Rest_Call():
-    response = requests.get("https://www.barchart.com/stocks/most-active/daily-volume-leaders")
-    print(response.json())
+    #response = requests.get("https://www.barchart.com/stocks/most-active/daily-volume-leaders")
+    response = requests.get("https://www.google.com/finance/quote/NIO:NYSE")
+    soup = BeautifulSoup(response.content,'html.parser')
+    print(soup)
 Making_Rest_Call()
 
 #def Send_SMS():
