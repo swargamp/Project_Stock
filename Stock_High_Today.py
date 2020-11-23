ecc02890e4e02b1f5e5adfcc2e68548fb3d23551
Macbook Pro <macbookpro@MacBook.local>
2020-11-22 18:01:01 -0800

    Added_Additional_urls

--- Stock_High_Today.py
+++ Stock_High_Today.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 
 def Making_Rest_Call():
     #response = requests.get("https://www.barchart.com/stocks/most-active/daily-volume-leaders")
-    response = requests.get("https://www.google.com/finance/quote/NIO:NYSE")
+    #response = requests.get("https://www.google.com/finance/quote/NIO:NYSE")
+    response = requests.get("https://gis.conservation.ca.gov/server/rest/services/CGS_Earthquake_Hazard_Zones/SHP_Liquefaction_Zones/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
     soup = BeautifulSoup(response.content,'html.parser')
     print(soup)
 Making_Rest_Call()
